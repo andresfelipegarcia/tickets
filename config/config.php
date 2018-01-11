42f23487a445c575b7770df97040af4a6f75aa7c
Andres Garcia <felipehonor@gmail.com>
2018-01-11 13:03:12 -0500

    Primer COmmit Tickets

---
+++ config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+	/*Datos de conexion a la base de datos*/
+	define('DB_HOST', 'localhost');//DB_HOST:  generalmente suele ser "127.0.0.1"
+	define('DB_USER', 'root');//Usuario de tu base de datos
+	define('DB_PASS', 'root');//Contraseña del usuario de la base de datos
+	define('DB_NAME', 'bd_tickets');//Nombre de la base de datos
+
+	$con=@mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+    if(!$con){
+    	print_r($con);
+        @die("<h2 style='text-align:center'>Imposible conectarse a la base de datos! </h2>".mysqli_error($con));
+    }
+    if (@mysqli_connect_errno()) {
+        @die("Conexión falló: ".mysqli_connect_errno()." : ". mysqli_connect_error());
+    }
+?>
